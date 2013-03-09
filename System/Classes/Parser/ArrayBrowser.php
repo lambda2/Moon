d6851b3179b3cd56e0e563342baacd960fb6fbb2
lambda2 <andre.aubin@lambdaweb.fr>
2013-03-09 20:50:08 +0100

    Ajout d'une classe ArrayBrowser permettant de convertir un tableau de tableaux (de tableaux de tableaux... etc) en un objet permettant facilement de parcourir la structure. Ainsi, pour le tableau
    	$options = array(
    	'info' => array (
    		'version' => string '0.0.1', 
    		'stable' => boolean false,
    		'supported_sql_drivers' =>string 'mysql'
    		)
    	)
    
    On peux acceder a la propriété $options['info']['stable'] en faisant simplement 
    	$options = ArrayBrowser($options)->info->stable;
    	$options->info->stable /* == $options['info']['stable'] */

---
+++ System/Classes/Parser/ArrayBrowser.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * This file is part of the Lambda Web Framework.
+ *
+ * (c) 2013 Lambdaweb - www.lambdaweb.fr
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Va nous permettre de naviguer simplement dans un ensemble de tableaux.
+ * @author lambda2
+ */
+class ArrayBrowser {
+    private $arr;
+    
+    /**
+     * Construit un ensemble d'options avec les données du tableau passé
+     * en parametre.
+     * @param type $tab le tableau contenant les données.
+     * @throws AlertException si le parametre n'est pas un tableau
+     */
+    public function __construct($tab){
+        if(!is_array($tab)){
+            throw new AlertException("le parametre de création d'une structure d'option doit etre un tableau !");
+        }
+        $this->arr = array();
+        
+        foreach ($tab as $key => $value) {
+            if(is_array($value)){
+            $this->arr[$key] = new ArrayBrowser($value);
+            }
+            else {
+                $this->arr[$key] = $value;
+            }
+        }
+    }
+    
+    
+    public function __get($name){
+        if(array_key_exists($name, $this->arr)){
+            return $this->arr[$name];
+        }
+        else {
+            throw new AlertException("La propriété $name n'est pas référencée...");
+        }
+    }
+    
+    /**
+     * Retourne un tableau comprenant les sous niveaux disponibles
+     * @return array un tableaux comprenant les sous niveaux disponibles
+     */
+    public function childs(){
+        $s = array();
+        foreach ($this->arr as $key => $value) {
+            $s[] = $key;
+        }
+        return $s;
+    }
+    
+    /**
+     * Retourne l'arborescence des options disponibles
+     * @return string l'arborescence des options disponibles
+     */
+    public function __toString(){
+        $s = "<ul>";
+        foreach ($this->arr as $key => $value) {
+            if(is_a($this->arr[$key], 'ArrayBrowser')){
+                $s .= '<li><b>'.$key.'</b> { '.$value.' } </li>';
+            }
+            else {
+                $s .= '<li>'.$key.' : '.$value.'</li>';
+            }
+        }
+        $s .= "</ul>";
+        return $s;
+    }
+}
+
+?>
