58f65889eb13fc538007f23de4059bf8ec431ec5
lambda2 <andre.aubin@lambdaweb.fr>
2013-03-10 11:35:44 +0100

    Séparation des classes Entity, EntityLoader et TableEntity dans un fichier pour chaque classe. (au lieu d'avoir les trois classes dans le meme fichier)

---
+++ System/Classes/Entity/EntityLoader.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the Lambda Web Framework.
+ *
+ * (c) 2013 Lambdaweb - www.lambdaweb.fr
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+class EntityLoader {
+
+    public static function getClass($className) {
+        $return = null;
+        try {
+            if (class_exists($className) && in_array('Entity', class_parents($className))) {
+                $return = new $className();
+            }
+            else {
+                //dbg("classe inexistante... scan de la base de données...");
+                $reducClassName = $className;
+
+                $withPrefix = strstr($reducClassName, Configuration::getInstance()->getDbPrefix());
+                //dbg("contient le prefixe ? ");
+                //echo $withPrefix ? 'true' : 'false';
+
+                if ($withPrefix != FALSE) {
+                    //echo "before : $reducClassName";
+                    $reducClassName = substr_replace($className, '', -(strlen(Configuration::getInstance()->getDbPrefix())), strlen(Configuration::getInstance()->getDbPrefix()));
+                    
+                    //echo "after : $reducClassName";
+                    
+                }
+
+                if (Configuration::isValidClass($reducClassName . Configuration::getInstance()->getDbPrefix())) {
+                    //dbg('isValidClass !  ('.$reducClassName.Configuration::getInstance()->getDbPrefix().')');
+                    
+                    $return = new TableEntity($reducClassName);
+                }
+                else if (Configuration::isValidClass($reducClassName)) {
+                    $return = new TableEntity($reducClassName);
+                }
+                else {
+                    throw new CriticalException('Impossible d\'instancier la classe ' . $reducClassName);
+                }
+            }
+        } catch (Exception $e) {
+            var_dump($e);
+        }
+        return $return;
+    }
+
+    /**
+     * Va [faire un essai pour] charger <b>une</b> instance <b>unique</b> de la
+     * classe spécifiée.
+     * @param String $fieldSchema le nom de la classe et du champ 
+     * a rechercher (de préférence un identifiant) sous la forme table.champ
+     * @param String $fieldValue la valeur de ce champ
+     * @return Mixed une instance de la classe
+     */
+    public static function loadInstance($fieldSchema, $fieldValue) {
+
+        $target = split('\.', $fieldSchema);
+        $class  = self::getClass($target[0]);
+
+        if ($target[1] != "" && $fieldValue != null) {
+            $class->loadBy($target[1], $fieldValue);
+        }
+        return $class;
+    }
+
+}
+
+?>
---
+++ System/Classes/Entity/TableEntity.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Lambda Web Framework.
+ *
+ * (c) 2013 Lambdaweb - www.lambdaweb.fr
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+/**
+ * Classe permettant d'instancier une classe a partir
+ * des valeurs d'une table spécifique
+ */
+class TableEntity extends Entity {
+
+    /**
+     * Crée une nouvelle instance référente à la table 
+     * fournie en parametre. On effectue préalablement 
+     * un scan des tables sur la base de données et on vérifie 
+     * que la table demandée existe. 
+     * Si ce n'est pas le cas, on jette une exception au visage 
+     * de ce pauvre internaute.
+     * 
+     * @param string $name le nom de la table référente
+     * @throws CriticalException si la table n'existe pas
+     */
+    public function __construct($name) {
+
+        // Si la table existe dans la bdd
+        if (Configuration::isValidClass($name . Configuration::getInstance()->getDbPrefix())) {
+
+            parent::__construct();
+            // On recharge
+            $this->reload($name);
+        }
+        else {
+            throw new CriticalException($name . ' n\'est pas une classe valide (non instanciable)');
+        }
+    }
+
+}
+
+?>
