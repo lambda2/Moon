cb80caa554e0605453bd089d8f8162a971b69f83
lambda2 <andre.aubin@lambdaweb.fr>
2013-03-10 16:50:03 +0100

    Debug de la page par défaut

--- Vues/accueil_m.php
+++ Vues/accueil_m.php
@@ -7,11 +7,5 @@ $var = $spyc->loadFile('System/Classes/Configuration/configuration.yml');
 $vardef = $spyc->loadFile('Config/access_defaults.yml');
 echo '<h3>var_dump($var)</h3>';
 var_dump($var);
-//echo '<h3>var_dump($vardef)</h3>';
-//var_dump($vardef);
-//echo '<h3>var_dump(array_merge_recursive($var,$vardef))</h3>';
-//var_dump(array_merge_recursive($vardef,$var));
-//echo '<h3>var_dump(array_merge_recursive_distinct($var,$vardef))</h3>';
-//var_dump(extendArray($var,$vardef));
 ?>
 
