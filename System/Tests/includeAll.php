e4971eb663037215ba8dc1557428c2a0589cbebc
André Aubin <andre.aubin@lambdaweb.fr>
2013-03-15 19:54:53 +0100

    Adding helper files for the include management of tests files. Adding basics unit tests for the Core class.

---
+++ System/Tests/includeAll.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Lambda Web Framework.
+ *
+ * (c) 2013 Lambdaweb - www.lambdaweb.fr
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$basePath = '/var/www/moon';
+
+
+include_once $basePath.'/System/Classes/Core/Core.php';
+include_once '/var/www/moon/System/Classes/Parser/Yaml/Spyc.php';
+include_once '/var/www/moon/System/Classes/Parser/ArrayBrowser.php';
+include_once '/var/www/moon/System/Helpers/common.php';
+include_once '/var/www/moon/System/Helpers/errors.php';
+include_once '/var/www/moon/System/Helpers/mail.php';
+include_once '/var/www/moon/System/Helpers/user.php';
+include_once '/var/www/moon/System/Classes/Debug/Debug.php';
+include_once '/var/www/moon/System/Classes/Router/Router.php';
+include_once '/var/www/moon/System/Classes/Exceptions/AlertException.php';
+
+
+
+
+
+?>
