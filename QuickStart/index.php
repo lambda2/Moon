4b4bbcc291105b69298d6d702a02ef9add834446
André Aubin <andre.aubin@lambdaweb.fr>
2013-03-25 19:01:23 +0100

    reorganize the strucutre of the application. Creating separated folders between the source and the system core

---
+++ QuickStart/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @TODO modifier cette horreur.
+ * > moteur de template ? (twig, smarty...)                     -   ✔
+ * > laisser le soin de faire le routage à une classe dédiée ?  -   ✔
+ * > système de binding d'url ?                                 -   ✔
+ * 
+ */
+
+// Pour vérifier a tout moment qu'on est dans l'index.php
+$INDEX = true;
+
+// on ajoute l'autoload des classes et on crée la connexion
+require_once('../System/loader.php');
+
+/**
+ * Enfin, on démarre notre moteur
+ */
+Core::startEngine();
+
+Core::route($_GET);
+
+?>
