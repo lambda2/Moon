0b29fb6ae4b5dd773f4a333d5a57501bc9e90de3
André Aubin <andre.aubin@lambdaweb.fr>
2013-04-23 23:39:48 +0200

    (Re) Adding the Quickstart demo project (pre-alpha0.0.01)

---
+++ QuickStart/Modeles/Astre.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Astre extends Entity
+{
+
+	public function __construct() {
+		echo '<!-- constructeur de Astre ! -->';
+		parent::__construct();
+	}
+
+
+    protected function setupFields(){
+		$this->editField('type')->setRequired(true);
+        $this->editField('systeme')->setRequired(true);
+        $this->getSysteme()->setLabelColumn('nom');
+    }
+}
+
+?>
