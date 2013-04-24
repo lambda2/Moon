02fe7f4ede8ffa165280c6988502441067a9aacc
André Aubin <andre.aubin@lambdaweb.fr>
2013-04-24 19:04:38 +0200

    Adding getters for POST, GET and SESSION + adding a security helpers file

---
+++ System/Helpers/security.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Manages all the security function who can
+ * help the user to prevent security failures.
+ */
+
+
+function md5sum($data)
+{
+	return hash('md5', $data);
+}
+
+function sha1sum($data)
+{
+	return hash('sha1', $data);
+}
+
+function sha256sum($data)
+{
+	return hash('sha256', $data);
+}
+
+function encrypt($data,$algo='sha256',$salt='')
+{
+	return hash($algo,$salt.$data.$salt);
+}
+
+
+?>
