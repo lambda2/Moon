0813c758babf83b69027f955e169293fbe69d7a3
André Aubin <andre.aubin@lambdaweb.fr>
2013-04-28 15:58:36 +0200

    Adding login methods for the MemberTemplate

--- System/Classes/Entity/EntityTemplates/MemberTemplate.php
+++ System/Classes/Entity/EntityTemplates/MemberTemplate.php
@@ -7,6 +7,38 @@ class MemberTemplate extends Entity {
     public function __construct() {
         parent::__construct();
     }
+
+    public static function isConnecte() {
+        return (isset($_SESSION['login']) ? true : false);
+    }
+
+    public function exists()
+    {
+
+    }
+
+    public function seConnecter() {
+        if ($this->exists()) {
+            $_SESSION['login'] = $this->pseudo;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function seDeconnecter() {
+        $_SESSION = array();
+
+		// le cookie de session.
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]
+            );
+        }
+
+		// Finalement, on détruit la session.
+        session_destroy();
+    }
     
 
 
